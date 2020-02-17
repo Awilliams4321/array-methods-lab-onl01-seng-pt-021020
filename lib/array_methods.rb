4ce84bd2cdf30cca036e0f2a38ea6688a263af1b
Amber Williams <awilliams1234@live.com>
2020-02-17 23:43:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,11 @@ array.include?(element)
 end
 
 def using_sort(array)
+<<<<<<< HEAD
 array.sort
+=======
+array.sort?(element)
+>>>>>>> 5b7e11b2fac48bba0d4e2e41750db781b55fc443
 end
 
 def using_reverse(array)
